64fee560b038ea2ba8187244c364958ba51763a5
bipblop555 <tom.bonnet@hotmail.com>
2025-10-14 17:34:12 +0200

    ajout des vm cat, niveau

---
+++ ConsulTech.Web/ConsulTech.Web/Models/ViewModels/Categories/CategorieListVm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsulTech.Web.Models.ViewModels.Categories;
+
+public class CategorieListVm
+{
+    public Guid Id { get; set; }
+
+    [Required, StringLength(25)]
+    public string Titre { get; set; } = string.Empty;
+}
