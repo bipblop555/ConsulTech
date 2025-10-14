c499ee1262c678eaf24ad442d31fdf0470270f64
bipblop555 <tom.bonnet@hotmail.com>
2025-10-14 17:33:44 +0200

    ajout des dto cat, client, comp, niveau

---
+++ ConsulTech.Web/ConsulTech.Web/Models/Dtos/Categorie/CategorieDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsulTech.Web.Models.Dtos.Categorie;
+
+public record CategorieDto()
+{
+    public Guid Id { get; set; }
+    public string Titre { get; set; } = string.Empty;
+};
---
+++ ConsulTech.Web/ConsulTech.Web/Models/Dtos/Niveau/NiveauDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsulTech.Web.Models.Dtos.Niveau;
+
+public record NiveauDto
+{
+    public Guid Id { get; set; }
+    public string Titre { get; set; } = string.Empty;
+}
