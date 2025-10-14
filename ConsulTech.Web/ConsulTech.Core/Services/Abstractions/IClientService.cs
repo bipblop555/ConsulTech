b9a7250045b1bb9ab747d4d99e448b28ddb22c77
bipblop555 <tom.bonnet@hotmail.com>
2025-10-14 14:42:48 +0200

    ajout client controller, service, dto

---
+++ ConsulTech.Web/ConsulTech.Core/Services/Abstractions/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using ConsulTech.Core.DTO;
+using ConsulTech.Core.Entities;
+
+namespace ConsulTech.Core.Services.Abstractions;
+
+public interface IClientService
+{
+    Task<List<Client>> GetAllClientsAsync();
+    Task<Client?> GetClientByIdAsync(Guid id);
+    Task<Guid> CreateClientAsync(ClientDto clientDto);
+    Task<Guid> UpdateClientAsync(ClientDto clientDto);
+    Task<bool> DeleteClientAsync(Guid id);
+}
---
+++ ConsulTech.Web/ConsulTech.Core/Services/ClientService.cs
@@ -0,0 +1,74 @@
+﻿using ConsulTech.Core.DTO;
+using ConsulTech.Core.Entities;
+using ConsulTech.Core.Services.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsulTech.Core.Services;
+
+internal sealed class ClientService : IClientService
+{
+    private readonly DbContext _dbContext;
+    public ClientService(DbContext dbContext)
+    {
+        this._dbContext = dbContext;
+    }
+    public async Task<Guid> CreateClientAsync(ClientDto clientDto)
+    {
+        //if (this._dbContext.Clients.Any(c => c.Nom == clientDto.Nom))
+        //    return Guid.Empty;
+
+        var clientToAdd = new Client
+        {
+            Nom = clientDto.Nom,
+            Secteur = clientDto.Secteur,
+            Adresse = clientDto.Adresse,
+            Contact = clientDto.Contact
+        };
+
+        //await this._dbContext.Clients.AddAsync(clientToAdd);
+        await this._dbContext.SaveChangesAsync();
+        return clientToAdd.Id;
+    }
+
+    public async Task<bool> DeleteClientAsync(Guid id)
+    {
+        //var foundClient = await this._dbContext.Clients.FirstOrDefaultAsync(c => c.Id == id);
+        //if(foundClient is null)
+            //return false;
+
+        //this._dbContext.Clients.Remove(foundClient);
+        await this._dbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<List<Client>> GetAllClientsAsync()
+    {
+        //return await this._dbContext.Clients.ToListAsync();
+        return new();
+    }
+
+    public async Task<Client?> GetClientByIdAsync(Guid id)
+    {
+        //return await this._dbContext.Clients.FirstOrDefaultAsync(c => c.Id == id);
+        return new Client();
+    }
+
+    public async Task<Guid> UpdateClientAsync(ClientDto clientDto)
+    {
+        //var foundClient = await this._dbContext.Clients.FirstOrDefaultAsync(c => c.Id == clientDto.Id);
+
+        //if (foundClient is null)
+        //    return Guid.Empty;
+
+        //foundClient.Nom = clientDto.Nom;
+        //foundClient.Secteur = clientDto.Secteur;
+        //foundClient.Adresse = clientDto.Adresse;
+        //foundClient.Contact = clientDto.Contact;
+
+        //await this._dbContext.Clients.UpdateAsync(foundClient);
+        await this._dbContext.SaveChangesAsync();
+        
+        //return foundClient.Id;
+        return Guid.Empty;
+    }
+}
