d64d52eb7544bde8389a6101f765eb52f0e1f64d
Eric Ghoubiguian <ericghoubiguian@live.fr>
2025-10-15 11:13:39 +0200

    Developments for mission

---
+++ ConsulTech.Web/ConsulTech.Api/Controllers/MissionController.cs
@@ -0,0 +1,79 @@
+﻿using ConsulTech.Api.Models;
+using ConsulTech.Core.DTO;
+using ConsulTech.Core.Services.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsulTech.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MissionController : ControllerBase
+    {
+        private readonly IMissionService _missionService;
+
+        public MissionController(IMissionService missionService)
+        {
+            _missionService = missionService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllMissionAsync()
+        {
+            return Ok(await this._missionService.GetAllMissionAsync());
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult> GetMissionByIdAsync(Guid id)
+        {
+            var foundMission = await this._missionService.GetMissionByIdAsync(id);
+            return foundMission is null ? NotFound() : Ok(foundMission);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateMissionAsync([FromBody] MissionInput mission)
+        {
+            var createdMissionId = await this._missionService.CreateMissionAsync(new MissionDto
+            {
+                Titre = mission.Titre,
+                Description = mission.Description,
+                Debut = mission.Debut,
+                Fin = mission.Fin,
+                Budget = mission.Budget
+            });
+
+            return createdMissionId != Guid.Empty ? Created($"/api/mission/{createdMissionId}", null) : Problem();
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<ActionResult> UpdateMissionAsync(Guid id, [FromBody] MissionInput mission)
+        {
+            if (id != mission.Id)
+                return BadRequest("L'id de la mission ne correspond pas à celui de l'URL.");
+
+            var updatedMissionId = await this._missionService.UpdateMissionAsync(new MissionDto
+            {
+                Id = mission.Id,
+                Description = mission.Description,
+                Debut = mission.Debut,
+                Fin = mission.Fin,
+                Budget = mission.Budget
+            });
+
+            return updatedMissionId != Guid.Empty ? NoContent() : Problem();
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteMissionAsync(Guid id)
+        {
+            var missionToDelete = await this._missionService.GetMissionByIdAsync(id);
+
+            if (missionToDelete is null)
+                return NotFound();
+
+            var isDeleted = await this._missionService.DeleteMissionAsync(missionToDelete.Id);
+
+            return isDeleted ? NoContent() : Problem();
+        }
+    }
+}
---
+++ ConsulTech.Web/ConsulTech.Api/Models/MissionInput.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsulTech.Api.Models
+{
+    public class MissionInput
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Titre { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(250)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Debut { get; set; }
+
+        [Required]
+        public DateTime Fin { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "Le budget doit être un nombre positif.")]
+        public float Budget { get; set; }
+    }
+}
